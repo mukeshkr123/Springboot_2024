04c5c8f7c4b4790f815bb8f45a19bca4f54d8aae
mukeshkr123 <mkemehta2041@gmail.com>
2024-05-26 11:28:38 +0530

    inital project created with start.spring.io

---
+++ firstjobApp/src/main/java/com/mukesh/firstjobApp/FirstjobAppApplication.java
@@ -0,0 +1,13 @@
+package com.mukesh.firstjobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjobAppApplication.class, args);
+	}
+
+}
---
+++ firstjobApp/src/test/java/com/mukesh/firstjobApp/FirstjobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukesh.firstjobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
